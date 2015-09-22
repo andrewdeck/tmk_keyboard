54b8acea6b8e94e3f239a60d126bf1b287d572cb
Andrew Deck <deck.andrew@gmail>
2015-09-21 23:51:13 -0400

    few more tweaks

--- keyboard/planck/keymaps/keymap_deck.c
+++ keyboard/planck/keymaps/keymap_deck.c
@@ -2,26 +2,31 @@
 #include "backlight.h"
 #include "debug.h"
 
+#define QWERTY_LAYER 0
+#define MODIFIER_LAYER 1
+#define FUNCTION_LAYER 2
+#define TENKEY_LAYER 3
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = { /* Qwerty */
+[QWERTY_LAYER] = { /* Qwerty */
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
   {M(0),    KC_LCTL, KC_LALT, KC_LGUI, FUNC(1), KC_SPC,  KC_SPC,  FUNC(2), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
-[1] = { /* Modifiers */
+[MODIFIER_LAYER] = { /* Modifiers */
   {KC_GRV,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_MINS,    S(KC_EQL), KC_LBRC,    KC_RBRC,  KC_BSLS},
   {S(KC_GRV), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),  S(KC_6),  S(KC_7),    S(KC_8),   S(KC_9),    S(KC_0), S(KC_BSLS)},
   {KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, FUNC(3), KC_TRNS,  KC_TRNS,  S(KC_MINS), KC_EQL,    S(KC_LBRC), S(KC_RBRC), KC_TRNS},
   {KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  FUNC(2),    KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS}
 },
-[2] = { /* Functions */
+[FUNCTION_LAYER] = { /* Functions */
   {KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_F12},
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,  KC_MPRV,  KC_MNXT,  KC_TRNS, KC_TRNS},
   {KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_VOLD,   KC_VOLU,   KC_MUTE,   KC_F10,  KC_TRNS},
-  {RESET, KC_TRNS, KC_TRNS, KC_TRNS, FUNC(1),   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_MPLY}
+  {RESET, KC_TRNS, KC_TRNS, KC_TRNS, FUNC(1),   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, FUNC(5), FUNC(6)}
 },
-[3] = { /* TENKEY */
+[TENKEY_LAYER] = { /* TENKEY */
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_P,   KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_SCLN,  KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_KP_1,    KC_KP_2,    KC_KP_3,  KC_SLSH, KC_ENT},
@@ -30,11 +35,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_MOMENTARY(2),  // to RAISE
-    [2] = ACTION_LAYER_MOMENTARY(3),  // to LOWER
+    [1] = ACTION_LAYER_MOMENTARY(MODIFIER_LAYER),
+    [2] = ACTION_LAYER_MOMENTARY(FUNCTION_LAYER),
+    [3] = ACTION_LAYER_MOMENTARY(TENKEY_LAYER),
 
-    [3] = ACTION_DEFAULT_LAYER_SET(0), 
     [4] = ACTION_DEFAULT_LAYER_SET(1),
+
+    [5] = ACTION_BACKLIGHT_DECREASE();
+    [6] = ACTION_BACKLIGHT_INCREASE();
 
 };
 
